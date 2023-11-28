23389c5446866f6469d414ddbd88d5c97317d3cf
JordiSegura <jordisegura1994@hotmail.com>
2023-11-28 19:45:04 +0100

    - Added new activity for carriers to act as welcomepage  - Created new button elements and onClick listener to open new activity based on selected option

---
+++ app/src/main/java/com/example/dam_m13_proyecto/CreateLoadCarrier.java
@@ -0,0 +1,6 @@
+package com.example.dam_m13_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateLoadCarrier extends AppCompatActivity {
+}
--- app/src/main/java/com/example/dam_m13_proyecto/WelcomePageCarrier.java
+++ app/src/main/java/com/example/dam_m13_proyecto/WelcomePageCarrier.java
@@ -7,24 +7,43 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.navigation.NavigationView;
 
-public class WelcomePageCarrier extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class WelcomePageCarrier extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private DrawerLayout drawer;
+
+    private Button buttonCreateLoad,buttonReviewCarrierRequests,buttonReviewOpenLoads;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomepage);
 
+        //DrawerLayout
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
+        drawer.open();
         toggle.syncState();
+
+        //Buttons
+        buttonCreateLoad = findViewById(R.id.buttonCreateLoad);
+        buttonCreateLoad.setOnClickListener(this);
+
+        buttonReviewCarrierRequests = findViewById(R.id.buttonReviewRequests);
+        buttonReviewCarrierRequests.setOnClickListener(this);
+
+        buttonReviewOpenLoads = findViewById(R.id.buttonOpenLoads);
+        buttonReviewOpenLoads.setOnClickListener(this);
+
     }
 
     @Override
@@ -44,6 +63,22 @@ public class WelcomePageCarrier extends AppCompatActivity implements NavigationV
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+
+        if (viewId == R.id.buttonCreateLoad) {
+            Intent intent = new Intent(WelcomePageCarrier.this, CreateLoadCarrier.class);
+            startActivity(intent);
+        } else if (viewId == R.id.buttonReviewRequests) {
+            Intent intent = new Intent(WelcomePageCarrier.this, ReviewCarrierRequests.class);
+            startActivity(intent);
+        } else if (viewId == R.id.buttonOpenLoads) {
+            Intent intent = new Intent(WelcomePageCarrier.this, ReviewOpenLoadsCarrier.class);
+            startActivity(intent);
         }
     }
 }
